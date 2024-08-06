532406e5a56f4ef52565aa8d27403590d302e662
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-06 11:20:07 -0600

    feat: crear seccion de header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <h1>Hello, dededde.js!</h1>
-  }
+import Image from "next/image";
+import styles from"./main.module.css"
+
+const Page = (): JSX.Element => {
+  return (
+    <main className={styles.container}>
+      <section className={styles.header}>
+        <Image src="/images/harry-potter.svg" alt="Logo" width={193} height={66} />
+        <h2 className={styles.textSelectFilter}>Selecciona tu filtro</h2>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
