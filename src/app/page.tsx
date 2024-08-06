dcb045e3bb74c47faa298eb3e6ac608c0a4b51fd
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-06 14:17:15 -0600

    feat: consumir info e iterar cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import styles from"./main.module.css"
+import CardComponent from "../components/card/card";
+import { Character } from "../types/character.types";
 
-const Page = (): JSX.Element => {
+const Page = async (): Promise<JSX.Element>  => {
+  const response: Response = await fetch('http://localhost:4000/characters');
+  const characters: Character[] = await response.json();
+
+  console.log(characters);
+
   return (
     <main className={styles.container}>
       <section className={styles.header}>
@@ -12,6 +19,23 @@ const Page = (): JSX.Element => {
       <section className={styles.containerBtns}>
         <button type="button" className={styles.btn}>ESTUDIANTES</button>
         <button type="button" className={styles.btn}>STAFF</button>
+      </section>
+
+      <section className={styles.containerCards}>
+        {characters.map((character) => (
+          <CardComponent
+            key={character.id}
+            name={character.name}
+            isAlive={character.alive}
+            birthDate={character.dateOfBirth}
+            gender={character.gender}
+            eyesColour={character.eyeColour}
+            hairColour={character.hairColour}
+            isStudent={character.hogwartsStudent}
+            image={character.image}
+            house={character.house}
+          />
+        ))}
       </section>
     </main>
   );
