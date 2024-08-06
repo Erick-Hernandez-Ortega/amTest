1a4b745c8607a6c2071aa5f23caa385ed500818d
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-06 11:29:17 -0600

    feat: crear seccion de botones de filtro, ademas de agregar sus estados en css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ const Page = (): JSX.Element => {
         <Image src="/images/harry-potter.svg" alt="Logo" width={193} height={66} />
         <h2 className={styles.textSelectFilter}>Selecciona tu filtro</h2>
       </section>
+
+      <section className={styles.containerBtns}>
+        <button type="button" className={styles.btn}>ESTUDIANTES</button>
+        <button type="button" className={styles.btn}>STAFF</button>
+      </section>
     </main>
   );
 };
