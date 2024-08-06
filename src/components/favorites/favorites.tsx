222392a1eee5e827def108cc0f5b19e70d1b4b7b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-06 15:50:12 -0600

    feat: crear menu desplegable con los favoritos

--- src/components/favorites/favorites.tsx
+++ src/components/favorites/favorites.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./styles.module.css"
 import React, { useState } from "react";
+import { Character } from "../../types/character.types";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFavorite } from "../../store/favoritesSlice";
+
 
 interface FavoriteProps {
 
@@ -8,6 +12,12 @@ interface FavoriteProps {
 
 const FavoriteComponent = (): React.ReactElement<FavoriteProps> => {
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+    const favorites: Character[] = useSelector((state: any) => state.favorites.items);
+    const dispatch = useDispatch();
+
+    const handleFavoriteRemove = (id: string) => {
+        dispatch(removeFavorite(id));
+    };
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -21,9 +31,15 @@ const FavoriteComponent = (): React.ReactElement<FavoriteProps> => {
                     <Image src="/icons/user-add.svg" alt="user-add-icon" width={20} height={22} />
                 </div>
             </div>
-            {modalIsOpen &&
+            {(modalIsOpen && favorites.length > 0) &&
                 <div className={styles.modal}>
-                    Modal
+                    {favorites.map((favorite: Character) => (
+                        <div key={favorite.id} className={styles.favorite}>
+                            <Image src={favorite.image} className={styles.image} alt="profile" width={34} height={34} />
+                            <p>{favorite.name}</p>
+                            <Image src='/icons/trash.svg' onClick={() => handleFavoriteRemove(favorite.id)} style={{ marginLeft: 'auto' }} alt="delete" width={24} height={24} />
+                        </div>
+                    ))}
                 </div>
             }
         </div>
